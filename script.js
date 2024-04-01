9aacfbefecd81b291542ed0f0cc65e39a2776a3f
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-01 18:51:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // Complete the js code
 function Car(make, model) {
-	constructor(make,model){
+	
 		this.make = make;
 		this.model = model;
-	}
-	getMakeModel(){
+		
+}
+Car.prototype.getMakeModel = function(){
 		return '${this.make} ${this.model}';
 	}
-}
 
 function SportsCar(make, model, topSpeed) {
-	constructor(make,model,topSpeed){
+	
 		this.make=make;
 		this.model = model;
 		this.topSpeed = topSpeed;
-	}
-	getTopSpeed(){
+}
+SportsCar.prototype.getTopSpeed = function(){
+	
 		return this.topSpeed;
-	}
+	
 }
 
 // Do not change the code below
